e0343a8ad23bc9cfb691065aaf6de0215bdbcdf7
Amanda Carbonari <acarb95@gmail.com>
2018-04-09 11:45:35 -0700

    Fixed silly bug in increment array test for bifrost

--- tutorial/c_glib/c_glib_client.c
+++ tutorial/c_glib/c_glib_client.c
@@ -580,7 +580,7 @@ void increment_array_perf(SimpleArrayComputationIf *client,
 
   fprintf(outfile, "size,us latency\n");
   for (int s = 5; s < max_size; s+= incr) {
-    increment_array_total += 0;
+    increment_array_total = 0;
     for (int i = 0; i < iterations; i++) {
       increment_array_total += test_increment_array(client, s, targetIP, FALSE);
        // increment_array_times[i];
